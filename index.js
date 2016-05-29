1fe70093b6d045f10782e55dd0648006dfe87312
puppy0cam <chawk37@eq.edu.au>
2016-05-29 19:15:40 +1000

    performed LINT on code
    cleaned up source

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-var Client = require('node-ssdp').Client
-var Finder = module.exports
-Finder._client = new Client();
-Finder._devices = []
-Finder._client.on('response', function (headers, statusCode, rinfo) {
-var Send = {
-'headers' : headers,
-'statusCode' : statusCode,
-'rinfo' : rinfo,
-'ip' : headers.LOCATION.slice(7,headers.LOCATION.length-10)
-}
-var Used = '"' + JSON.stringify(Send) + '"'
+var Client = require('node-ssdp').Client;
+var Finder = module.exports = function () {
+	if (Finder.client === undefined) {
+		Finder.client = new Client();
+		Finder.devices = [];
+		Finder.client.on('response', function (headers, statusCode, rinfo) {
 
-if(Used.includes("Belkin") === true) {
-Finder._devices[Finder._devices.length] = Send
-console.log('\x1b[44m\x1b[33m<<<\x1b[32m WEMO DEVICE FOUND \x1b[33m>>>\x1b[0m');
-console.log('\x1b[44m\x1b[33m<<<\x1b[32m SAVED INFO \x1b[33m>>>\x1b[0m');
-}
-});
-// search for a service type
-Finder._client.search("urn:Belkin:device:controllee:1");
-console.log('\x1b[44m\x1b[33m<<<\x1b[32m LOADED FINDER \x1b[33m>>>\x1b[0m');
+				var Send = {
+				'headers': headers,
+				'statusCode': statusCode,
+				'rinfo': rinfo,
+				'ip': headers.LOCATION.slice(7, headers.LOCATION.length - 10)
+			};
+
+			Finder.devices[Finder.devices.length] = Send;
+			console.log('\x1b[44m\x1b[33m<<<\x1b[32m WEMO DEVICE FOUND \x1b[33m>>>\x1b[0m');
+			console.log('\x1b[44m\x1b[33m<<<\x1b[32m IP: ' + Send.ip + ' \x1b[33m>>>\x1b[0m');
+		});
+
+		// search for a service type
+		Finder.client.search("urn:Belkin:device:controllee:1");
+		console.log('\x1b[44m\x1b[33m<<<\x1b[32m LOADED FINDER \x1b[33m>>>\x1b[0m');
+		return "made by puppy0cam A.K.A. Cameron";
+	} else {
+		Finder.devices = [];
+		Finder.client.search("urn:Belkin:device:controllee:1");
+	}
+};
